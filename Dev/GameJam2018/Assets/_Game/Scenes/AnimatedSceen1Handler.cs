89c69e7b550ecf9c2b7b633423a17acd3c57cf99
Bartal Clementsen <bac@formula.fo>
2018-09-29 17:46:32 +0100

    Added first animation Scene

---
+++ Dev/GameJam2018/Assets/_Game/Scenes/AnimatedSceen1Handler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AnimatedSceen1Handler : MonoBehaviour
+{
+   
+    // Use this for initialization
+    private void Start()
+    {
+        StartCoroutine(ToNextSceenAfterDelay());
+    }
+
+    private IEnumerator ToNextSceenAfterDelay()
+    {
+
+        yield return new WaitForSeconds(14);
+        SceneManager.LoadScene(2);
+    }
+
+}
